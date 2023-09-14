951ae03734fa57817b61727b4fb5611042f7b067
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2023-09-13 22:50:49 -0300

    corrigindo o 19

--- robertinho/list-1/ex19.js
+++ robertinho/list-1/ex19.js
@@ -1,5 +1,7 @@
 // Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. 
 // O resultado da operação deve ser acompanhado de uma frase que diga se o número e:
+// Par ou ímpar;
+// Positivo ou negativo;
 
 const prompt = require("prompt-sync");
 const ask = prompt();
@@ -11,8 +13,6 @@ const operacao = ask("Qual operação você gostaria de realizar?R.: ");
 const a = Number(numeroUm);
 const b = Number(numeroDois);
 let resultado = 0;
-let resto = resultado % 2;
-let parImpar = 0;
 
 if (operacao == "soma" || operacao == "adição" || operacao == "+") {
     resultado = a+b;
@@ -33,6 +33,8 @@ if (operacao == "dividir" || operacao == "divisão" || operacao == "/") {
     resultado = a/b;
     console.log(`O resultado é: ${resultado}`);
 }
+
+let resto = resultado % 2;
 
 if (resto == 0) {
     console.log("Par");
