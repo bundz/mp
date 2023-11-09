d45979c88d7d1b2c2d83a6c0a20f21e7529c025d
Roberto Ferreira <rrobertonunesferreira@gmail.com>
2023-11-09 18:53:50 -0300

    ex0

--- robertinho/list-3/ex0.js
+++ robertinho/list-3/ex0.js
@@ -6,21 +6,31 @@ const ask = prompt();
 let inicio = ask("Você gostaria de inserir um cpf? ");
 
 let soma = 0;
+let cadastros = [];
+let validade = 'Inválido';
 
 while (inicio == "sim") {
+    let nome = ask("Informe o nome: ");
     let cpf = ask("Informe o cpf: ");
-
-    if (cpf.length < 11 || cpf.length > 11) {
-        console.log(`Dados incorretos`);
-    }
 
     for (let i = 0; i < cpf.length; i ++) {
         soma = soma + Number(cpf[i]);
     }
 
     if (soma % 2 == 0) {
-        console.log(`${cpf} é um CPF válido!`);
+        validade = 'Válido';
     }
+
+    if (cpf.length < 11 || cpf.length > 11) {
+        validade = 'Inválido';
+    }
+
+    cadastros.nome = nome;
+    cadastros.cpf = cpf;
+    cadastros.validade = validade;
+
 
     inicio = ask("Você gostaria de inserir um cpf? ");
 }
+
+console.log(cadastros);
