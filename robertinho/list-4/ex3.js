d6b1a01f24aced40e405fa3410c8e82c70a60bb0
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2023-11-16 21:04:16 -0300

    ex4

--- robertinho/list-4/ex3.js
+++ robertinho/list-4/ex3.js
@@ -1,2 +1,19 @@
 //Faça uma função que recebe por parâmetro um valor inteiro e positivo e retorna o 
 //valor lógico Verdadeiro caso o valor seja primo e Falso em caso contrário.
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+function isPrime (number) {
+    for (let divisor = 2; divisor < number; divisor ++) {
+        if (number % divisor == 0) return false;
+    }
+    return true
+
+}
+
+const numero = ask("Informe um numero: ");
+
+const number = Number(numero);
+
+console.log(isPrime(number));
