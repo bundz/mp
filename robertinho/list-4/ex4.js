25545ea6c9fcf69dd16b6ed3100c6c0dbcfd6ca3
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2023-11-30 02:04:23 -0300

    ex 4

--- robertinho/list-4/ex4.js
+++ robertinho/list-4/ex4.js
@@ -1,2 +1,35 @@
 //Faça um procedimento que recebe por parâmetro os valores necessário para o cálculo da 
 //fórmula de báskara e retorna,também por parâmetro, as suas raízes, caso seja possível calcular.
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+const a = ask("Informe o termo 'a': ");
+const b = ask("Informe o termo 'b': ");
+const c = ask("Informe o termo 'c': ");
+
+let raiz1 = 0;
+let raiz2 = 0;
+let delta = 0;
+
+function bhaskara (a,b,c) {
+    delta = ((b * b) - 4 * a * c);
+
+    raiz1 = ((- b + Math.sqrt(delta))/(2*a));
+
+    raiz2 = ((- b - Math.sqrt(delta))/(2*a));
+
+    if (delta < 0) {
+        return (`O conjunto de dados não apresenta raízes reais`);
+    }
+
+    if (delta == 0) {
+        return (`Conjunto de dados apresenta uma única raiz = ${raiz1}`);
+    }
+
+    if (delta > 0) {
+        return (`As raízes são: ${raiz1} e ${raiz2}`);
+    }
+}
+
+console.log(bhaskara(a,b,c));
--- robertinho/list-4/ex6.js
+++ robertinho/list-4/ex6.js
@@ -1,1 +1,7 @@
 //Faça uma função que recebe a idade de uma pessoa em anos, meses e dias e retorna essa idade expressa em dias.
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+const idade = ask("Informe a idade (anos): ");
+
