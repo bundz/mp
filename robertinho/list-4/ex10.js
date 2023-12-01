b1a708aa5b4c9c03bbdffd2ae7ca1ce9c27a8ec2
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2023-12-01 01:09:38 -0300

    9-10-12(incompleto)

--- robertinho/list-4/ex10.js
+++ robertinho/list-4/ex10.js
@@ -1,2 +1,16 @@
 //Faça uma função que recebe um valor inteiro e verifica se o valor é par ou ímpar. 
 //A função deve retornar um valor booleano.
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+const number = ask("Digite um número: ");
+
+function isPositive (number) {
+    if (number % 2 == 0) {
+        return true
+    }
+    return false
+}
+
+console.log (isPositive(number));
--- robertinho/list-4/ex9.js
+++ robertinho/list-4/ex9.js
@@ -1,2 +1,16 @@
 //Faça uma função que recebe um valor inteiro e verifica se o valor é positivo ou negativo. 
 //A função deve retornar um valor booleano.
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+const number = ask("Digite um número: ");
+
+function isPositive (number) {
+    if (number > 0) {
+        return true
+    }
+    return false
+}
+
+console.log (isPositive(number));
