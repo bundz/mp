04d6c8dda37143e768233b9f5254869313ae0bd2
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2023-12-05 00:45:45 -0300

    ex 8

--- robertinho/list-4/ex8.js
+++ robertinho/list-4/ex8.js
@@ -6,3 +6,33 @@
 //11-13 anos	                    Juvenil A
 //14-17 anos	                    Juvenil B
 //Maiores de 18 anos (inclusive)	Adulto
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+const age = ask("Informe a idade (anos): ");
+
+function swimmerCategory (age) {
+    if (age < 5) {
+        return 'Não atingiu a idade mínima';
+    }
+    
+    if (age >= 5 && age <= 7) {
+        return 'Infantil A';
+    }
+    if (age >= 8 && age <= 10) {
+        return 'Infantil B';
+    }
+    if (age >= 11 && age <= 13) {
+        return 'Juvenil A';
+    }
+    if (age >= 14 && age <= 17) {
+        return 'Juvenil B';
+    }
+    if (age >= 18) {
+        return 'Adulto';
+    }
+    
+}
+
+console.log(swimmerCategory(age));
