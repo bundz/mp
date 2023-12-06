e5cdcc6c29ec8d546f3659ca4fd614ad2fdff9ec
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2023-12-05 23:54:22 -0300

    ex 11

--- robertinho/list-4/ex11.js
+++ robertinho/list-4/ex11.js
@@ -5,3 +5,22 @@
 // de 5,0 a 6,9	        C
 // de 7,0 a 8,9	        B
 // de 9,0 a 10,0	    A
+
+const prompt = require("prompt-sync");
+const ask = prompt();
+
+const average = ask("Digite a média do aluno: ");
+
+function concept (average) {
+    if (average >= 0 && average < 5) {
+        return 'D';
+    }    if (average >= 5 && average < 7) {
+        return 'C';
+    }    if (average >= 7 && average < 9) {
+        return 'B';
+    }    if (average >= 9 && average <= 10) {
+        return 'A';
+    }
+}
+
+console.log(concept (average));
